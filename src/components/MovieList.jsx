191b85a309c4f27e33efd43e4616b1b6274ff706
BabarAslamm <babaraslam825@gmail.com>
2025-12-20 21:54:32 +0500

    MovieList and MovieCard components added

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+function MovieList() {
+  return (
+        <div className="movie-list">
+            <MovieCard />
+        </div>
+    )
+}
+
+export default MovieList
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MovieList from '../components/MovieList'
 
 function Home() {
   return (
@@ -7,26 +8,7 @@ function Home() {
             <input className="searchInput" placeholder="Search for a movie..." />
             <button type="submit">Search 🔎</button>
           </form>
-          <div className="movie-list">
-            <div className="movie-card">
-              <img alt="The Avengers" src="https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg" />
-              <h3>The Avengers</h3>
-              <p>2012</p>
-              <a href="movie-detail.html" data-discover="true">Details</a>
-            </div>
-            <div className="movie-card">
-              <img alt="The Avengers" src="https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg" />
-              <h3>The Avengers</h3>
-              <p>2012</p>
-              <a href="movie-detail.html" data-discover="true">Details</a>
-            </div>
-            <div className="movie-card">
-              <img alt="The Avengers" src="https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg" />
-              <h3>The Avengers</h3>
-              <p>2012</p>
-              <a href="movie-detail.html" data-discover="true">Details</a>
-            </div>
-          </div>
+          <MovieList />
         </div>
   )
 }
